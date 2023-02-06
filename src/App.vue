48fb7ef229983544b863022e9ff5d4793e3f39d9
Daniel Abernathy <dabernathy89@gmail.com>
2023-02-06 11:15:25 -0600

    initial commit (stackblitz download)

---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<!-- Fetch the contents of https://api.publicapis.org/entries and display the first 10 entries in a table. Once the table is working, visually indicate odd rows. -->
+
+<script setup lang="ts"></script>
+
+<template></template>
